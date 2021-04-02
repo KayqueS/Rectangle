3f13e02075d570cc9dd3b9812a538eb1065ae4ef
KayqueS <kayque.sanmartin65@gmail.com>
2021-04-02 10:25:58 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle x = new Rectangle();
+
+            Console.WriteLine("Enter the width and height of the rectangle: ");
+            x.Width = double.Parse(Console.ReadLine());
+            x.Height = double.Parse(Console.ReadLine());
+            Console.WriteLine($"AREA = {x.Area():F2}");
+            Console.WriteLine($"PERIMETER = {x.Perimeter():F2}");
+            Console.WriteLine($"DIAGONAL = {x.Diagonal():F2}");
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace rectangle
+{
+    class Rectangle
+    {
+        public double Width;
+        public double Height;
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Width, 2.0) + Math.Pow(Height, 2.0));
+        }
+    }
+}
